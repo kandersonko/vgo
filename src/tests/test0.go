b3c3b72e5b6e910729b0f5ef55e908d6defe4f17
kandersonko <kandersonko@gmail.com>
2019-10-16 01:46:11 -0700

    Fix package main conflicts with function main

--- src/tests/test0.go
+++ src/tests/test0.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 type pet struct {
@@ -19,13 +18,20 @@ var z, t int
 var logicalValue bool
 
 func main() {
-	var year = math.Sin(20)
+	// var year = math.Sin(20)
 	print("hello, world\n")
 	fmt.Println("stuff and things")
-	var m string = "hello"
+	var m string
+	var age int
+	// const age int = 17
 }
 
 func add(a int, b float64) string {
 	var k string
 	return a + b
+}
+
+func double(n int) float64 {
+	var factor float64
+	return n * factor
 }
